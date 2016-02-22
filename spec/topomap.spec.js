2669124b7f3dd55b3b02ec0a1d48d8de751ede5b
mroberge <mroberge.whois@gmail.com>
2016-02-21 20:53:07 -0500

    Clean up. Changed tabs to spaces.

--- spec/topomap.spec.js
+++ spec/topomap.spec.js
@@ -1,33 +1,34 @@
-describe("topoMap", function() {
-	it("should load properly", function() {
-		expect(update).toEqual(jasmine.any(Function));
-	});
+describe("topoMap", function () {
+    it("should load properly", function () {
+        expect(update).toEqual(jasmine.any(Function));
+    });
 });
 
-describe("The UI functions", function() {
-	beforeEach(function() {
-		$('<div id="toddler"></div>').appendTo('body');
-	});
+describe("The UI functions", function () {
+    beforeEach(function () {
+        $('<div id="toddler"></div>').appendTo('body');
+    });
 
-	afterEach(function() {
-		$('#toddler').remove()
-	});
+    afterEach(function () {
+        $('#toddler').remove()
+    });
 
-	it("should contain get()", function() {
-		expect(get).toEqual(jasmine.any(Function));
-	});
-	it("...which should get elements from the DOM", function() {
-		expect(get("toddler")).toEqual(document.getElementById('toddler'));
-		expect(get("toddler")).toBe(document.getElementById('toddler'));
-	});
-	it("should contain update()", function() {
-		expect(update).toEqual(jasmine.any(Function));
-	});
-	it("...which should return the element it is working on", function(){
-		var elem = document.getElementById('toddler');
-		expect(update(elem, "new text")).toEqual(document.getElementById('toddler'));
-	});
-	xit("...which should add text to elements from the DOM", function() {
+    it("should contain get()", function () {
+        expect(get).toEqual(jasmine.any(Function));
+    });
+    it("...which should get elements from the DOM", function () {
+        expect(get("toddler")).toEqual(document.getElementById('toddler'));
+        expect(get("toddler")).toBe(document.getElementById('toddler'));
+    });
+    it("should contain update()", function () {
+        expect(update).toEqual(jasmine.any(Function));
+    });
+    it("...which should return the element it is working on", function () {
+        var elem = document.getElementById('toddler');
+        expect(update(elem, "new text")).toEqual(document.getElementById('toddler'));
+    });
+    xit("...which should add text to elements from the DOM", function () {
 
-	});
+    });
+
 });
